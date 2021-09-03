53264a066bd95e34d2fa528eedad95fa12e1d648
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-09-03 11:00:53 -0300

    Entrada de dados da Entrega Minima

--- src/br/com/zup/DesafioModulo1_2.java
+++ src/br/com/zup/DesafioModulo1_2.java
@@ -1,14 +1,39 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class DesafioModulo1_2 {
 
     public static void main(String[] args) {
+        //Instanciando Scanner
+        Scanner leitor = new Scanner(System.in);
 
         //ENTREGA MINIMA*********************
-        //VARIAVEIS
+        //Entrega Mínima: O sistema permite, via terminal,
+        // inserir pelo menos 1 funcionário nome, telefone,
+        // email e CPF.
+        //em uma lista e retornar o mesmo na tela.
 
+        //VARIAVEIS
+        String nomeFuncionario = "";
+        String telefoneFuncionario = "";
+        String emailFuncionario = "";
+        String cpfFuncionario = "";
 
         //ENTRADA DADOS DO USUARIO
+        System.out.printf("\n******* CADASTRO DE FUNCIONARIOS *******\n");
+
+        System.out.printf("\nPor favor, digite o nome do funcionario: \n");
+        nomeFuncionario = leitor.nextLine();
+
+        System.out.printf("\nPor favor, digite o CPF do funcionario: \n");
+        cpfFuncionario = leitor.nextLine();
+
+        System.out.printf("\nPor favor, digite o email do funcionario: \n");
+        emailFuncionario = leitor.nextLine();
+
+        System.out.printf("\nPor favor, digite o númerode telefone do funcionario: \n");
+        telefoneFuncionario = leitor.nextLine();
 
 
         //EXIBIR LISTA
