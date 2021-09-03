a3797efe3086a0efd2c830a81a65da44a8ce6087
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-09-03 11:12:36 -0300

    Entrada dados a cadastrar e print na tela da Entrega Minima

--- src/br/com/zup/DesafioModulo1_2.java
+++ src/br/com/zup/DesafioModulo1_2.java
@@ -1,12 +1,16 @@
 package br.com.zup;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class DesafioModulo1_2 {
 
     public static void main(String[] args) {
-        //Instanciando Scanner
+        //Instanciando   o contruido
         Scanner leitor = new Scanner(System.in);
+        Map<String, String> cadastro = new HashMap<String, String>();
+
 
         //ENTREGA MINIMA*********************
         //Entrega Mínima: O sistema permite, via terminal,
@@ -32,12 +36,15 @@ public class DesafioModulo1_2 {
         System.out.printf("\nPor favor, digite o email do funcionario: \n");
         emailFuncionario = leitor.nextLine();
 
-        System.out.printf("\nPor favor, digite o númerode telefone do funcionario: \n");
+        System.out.printf("\nPor favor, digite o número de telefone do funcionario: \n");
         telefoneFuncionario = leitor.nextLine();
+
+        //Almacenando od dados
+        cadastro.put("O CPF: " + cpfFuncionario, " Funcionario: " + nomeFuncionario + " E-mail: " + emailFuncionario + " Telefone: " + telefoneFuncionario);
 
 
         //EXIBIR LISTA
-
+        System.out.println(cadastro);
 
     }
 }
