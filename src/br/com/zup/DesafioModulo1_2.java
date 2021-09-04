d07b723f18e7bc6d51b59f44a38a15dbfc7065c3
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-09-04 11:21:56 -0300

    Trocando Estrutura If por SWITCH e adicionando verificação

--- src/br/com/zup/DesafioModulo1_2.java
+++ src/br/com/zup/DesafioModulo1_2.java
@@ -1,5 +1,6 @@
 package br.com.zup;
 
+import java.security.Key;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -21,10 +22,10 @@ public class DesafioModulo1_2 {
         String nomeFuncionario;
         String telefoneFuncionario;
         String emailFuncionario;
-        String cpfFuncionario;
+        String cpfFuncionario = "";
         int menu = 1;
         int menuopcao;
-        String deletar;
+
 
         //menu
         while (menu != 4) {
@@ -38,72 +39,85 @@ public class DesafioModulo1_2 {
             leitor.nextLine();
 
             //[1] Adicionar Funcionarios
-            if (menuopcao == 1) {
-                System.out.println("[1] Adicionar Funcionarios");
-                //ENTRADA DADOS DO USUARIO
-                System.out.println("\n******* CADASTRO DE FUNCIONARIOS *******");
-
-                System.out.println("\nPor favor, digite o nome do funcionario: ");
-                nomeFuncionario = leitor.nextLine();
-
-                System.out.println("\nPor favor, digite o CPF do funcionario: ");
-                cpfFuncionario = leitor.nextLine();
-
-                System.out.println("\nPor favor, digite o email do funcionario:");
-                emailFuncionario = leitor.nextLine();
-
-                System.out.println("\nPor favor, digite o número de telefone do funcionario: ");
-                telefoneFuncionario = leitor.nextLine();
-
-                //Almacenando os dados
-                cadastro.put("O CPF: " + cpfFuncionario, " Funcionario: " + nomeFuncionario + " E-mail: " + emailFuncionario + " Telefone: " + telefoneFuncionario);
-                System.out.println("\nCadastro finalizado com Suceso");
 
 
-            } else if (menuopcao == 2) {
-                //[2] Exibir Lista de Funcionarios
+            switch (menuopcao) {
+                case 1:
+                    System.out.println("[1] Adicionar Funcionarios");
+                    //ENTRADA DADOS DO USUARIO
+                    System.out.println("\n******* CADASTRO DE FUNCIONARIOS *******");
 
-                System.out.println("\n[2] Exibir Lista de Funcionarios");
+                    System.out.println("\nPor favor, digite o nome do funcionario: ");
+                    nomeFuncionario = leitor.nextLine();
 
-                // validando para percorrer a lista
-                if (cadastro.isEmpty()) {
+                    System.out.println("\nPor favor, digite o CPF do funcionario: ");
+                    cpfFuncionario = leitor.nextLine();
 
-                    System.out.println("\nPor Favor Adicione Funcionarios a su cadastro");
+                    System.out.println("\nPor favor, digite o email do funcionario:");
+                    emailFuncionario = leitor.nextLine();
 
-                } else {
+                    System.out.println("\nPor favor, digite o número de telefone do funcionario: ");
+                    telefoneFuncionario = leitor.nextLine();
 
-                    for (String referencia : cadastro.keySet()) {
-                        System.out.println(referencia+cadastro.get(referencia));
+
+                    if (cadastro.size() != 0) {
+                        for (String verificar : cadastro.keySet()) {
+                            if (verificar.equals(cpfFuncionario)) {
+                                System.out.println("Este CPF já está cadastrado");
+                            } else {
+                                cadastro.put(cpfFuncionario, " Funcionario: " + nomeFuncionario + " E-mail: " + emailFuncionario + " Telefone: " + telefoneFuncionario);
+                                System.out.println("\nCadastro finalizado com Suceso");
+                            }
+                        }
+                    } else {
+                        //Almacenando os dados
+                        cadastro.put(cpfFuncionario, " Funcionario: " + nomeFuncionario + " E-mail: " + emailFuncionario + " Telefone: " + telefoneFuncionario);
+                        System.out.println("\nCadastro finalizado com Suceso");
+
                     }
-                }
+                    break;
 
-            } else if (menuopcao == 3) {
-                //[3] Excluir Funcionarios
+                case 2:
+                    System.out.println("\n[2] Exibir Lista de Funcionarios");
 
-                System.out.println("\n[3] Excluir Funcionarios");
+                    // validando para percorrer a lista
+                    if (cadastro.isEmpty()) {
 
-                if (cadastro.isEmpty()) {
-                    System.out.println("Não existe ningum cadastro para ser deletado");
-                } else {
-                    System.out.println("Por favor, Digite o CPF do funcionario a ser removido: ");
-                    deletar = leitor.nextLine();
+                        System.out.println("\nPor Favor Adicione Funcionarios a su cadastro");
 
-                    cadastro.remove(deletar);
-                    System.out.println("\nCadastro removido com suceso");
+                    } else {
 
-                }
+                        for (String referencia : cadastro.keySet()) {
+                            System.out.println(referencia + cadastro.get(referencia));
+                        }
+                    }
+                    break;
 
-            } else if (menuopcao == 4) {
-                //[3] Sair do Programa
-                System.out.println("[4] Sair do Programa\n");
-                menu = 4;
+                case 3:
+                    System.out.println("\n[3] Excluir Funcionarios");
+                    System.out.println("\nPor favor digite o CPF do funcionario a excluir: ");
+                    String excluir = leitor.nextLine();
 
-                //Quando o usuario introduz um valor diferente ao mostrado no menu
+                    if (cpfFuncionario.isEmpty()) {
+                        System.out.println("Não tem cadastro para ser excluidos");
+                    } else {
+                        if (excluir.equals(cpfFuncionario)) {
+                            cadastro.remove(excluir);
+                        }
+                    }
+                    break;
 
-            } else {
-                System.out.println("Marque uma opçao valida");
+                case 4:
+                    System.out.println("[4] Sair do Programa\n");
+                    menu = 4;
+
+                    //Quando o usuario introduz um valor diferente ao mostrado no menu
+
+                    break;
+
+                default:
+                    System.out.println("Marque uma opçao valida");
             }
-           // cadastro.remove(deletar);
         }
     }
 }
